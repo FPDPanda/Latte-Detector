b56dc5f8df596fa29b91260eb8c6fd219dd1571a
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-02-10 15:06:07 -0300

    Tracking selected inputs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-function selectedMilk(milk) {
-    if (milk === "dairyMilk") {
-        return alert('You have added dairy milk!')
-    }
-    else if (milk === "soyMilk") {
-        return alert('You have added soy milk!')
-    }
-    else {
-    return alert('You have added cream!')
-    }
-}
+var latte = {
+    selectedMilk: "",
+    selectedEspresso: "",
 
-function selectedEspresso(espresso) {
-    if (espresso === 'caffeinatedEspresso'){
-        return alert("You have added caffeinated espresso!")
-    }
-    return alert("You have added decaf espresso!")
-}
+    selectMilk(milk) {
+        this.selectedMilk = milk;
 
-function chocolate(){
-    return alert("If you add chocolate it won't be a latte!")
+        alert(`You have added ${milk}!`);
+    },    
+    selectEspresso(espresso) {
+        this.selectedEspresso = espresso;
+
+        alert(`You have added ${espresso}!`);
+    },    
+    chocolate() {
+        alert("If you add chocolate it won't be a latte!")
+    }
 }
